4e6054f493e25b27c8258c7ac762ae1af635e208
PowderedMilk <willwolf3@gmail.com>
2015-06-16 05:17:18 -0400

    0
    1

--- js/z.js
+++ js/z.js
@@ -37,12 +37,14 @@ newObject("http://cdn.frankerfacez.com/emoticon/29455/1",28,25);
 
 
 //////////////////////////////////////////////
-
+window.dogsHtml = "";
 function winSize(){winWidth=(moz)?window.innerWidth:document.body.clientWidth;winHeight=(moz)?window.innerHeight:document.body.clientHeight;}
 function winOfy(){winOffset=(moz)?window.pageYOffset:document.body.scrollTop;}
 function fallObject(num,vari,nu){
-	objects[num]=new Array(parseInt(Math.random()*(winWidth-waft)),-30,(parseInt(Math.random()*waft))*((Math.random()>0.5)?1:-1),0.02+Math.random()/20,0,1+parseInt(Math.random()*fallSpeed),vari,fallObjects[vari][1],fallObjects[vari][2]);
-	if(nu==1){document.write('<img id="fO'+i+'" style="position:absolute;" src="'+fallObjects[vari][0]+'">'); }
+	objects[num] = new Array(parseInt(Math.random()*(winWidth-waft)),-30,(parseInt(Math.random()*waft))*((Math.random()>0.5)?1:-1),0.02+Math.random()/20,0,1+parseInt(Math.random()*fallSpeed),vari,fallObjects[vari][1],fallObjects[vari][2]);
+	if (nu==1) {
+		window.dogsHtml += '<img id="fO' + i + '" style="position:absolute;" src="'+fallObjects[vari][0]+'">';
+	}
 }
 function fall(){
 	for(i=0;i<numObjs;i++){
@@ -61,6 +63,8 @@ function rain() {
 	for (i=0;i<numObjs;i++) {
 		fallObject(i,parseInt(Math.random()*fallObjects.length),1);
 	}
-	window.onscroll=winOfy;window.onresize=winSize;
+	window.onscroll = winOfy;
+	window.onresize = winSize;
+	document.getElementById('dogs').innerHTML = window.dogsHtml;
 	fall();
 }
